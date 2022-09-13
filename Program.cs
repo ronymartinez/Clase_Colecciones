3c3bb332606c59d9bff622d59a1f30f6cbf4ec7a
ronym <ronym@WIN-2V16CPMQ3O2>
2022-09-12 22:04:58 -0300

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Biblioteca;
+
+namespace Clase_06
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Diccionario");
+        }
+    }
+}
